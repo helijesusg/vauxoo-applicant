cef0c5d3a65fb90690afa822292460f37d406389
HeliJesusG <helijesusg@gmail.com>
2016-11-08 15:46:08 -0400

    py2

--- calculator.py
+++ calculator.py
@@ -8,7 +8,6 @@ class CalculatorClass(object):
 	"""
 	Class used to calculate the sum of a list of numbers.
 	"""
-
 	def sum(self, num_list):
 		"""
 		Method to sum the list of numbers that is received in num_list
