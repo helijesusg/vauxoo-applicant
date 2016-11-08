7ef9a145b040b0173a80e04106bf7a90d681bcce
HeliJesusG <helijesusg@gmail.com>
2016-11-08 13:27:11 -0400

    py2

--- calculator.py
+++ calculator.py
@@ -1,16 +1,28 @@
 """
-Your module documentation here
+TECHNICAL EVALUATION - VAUXOO
+
+DEVELOPED BY:	HELI JESUS GONZALEZ TORRES
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    Class used to calculate the sum of a list of numbers.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Method to sum the list of numbers that is received in num_list
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+		sum = 0
+		for i in range(0,len(num_list)):
+			sum += num_list[i]
+		
+        return sum		# Remove this dummy line
+
+# Code section to test the class through a console
+calculator = CalculatorClass()							# The class is instantiated
+value = calculator.sum([1,2,3,4,5,6])					# Class method is called
+print value												# Console output output
+del calculator											# Instance is deleted
