307b19d6408ad4b5f1e3b588b93c2be4a29b6cc8
HeliJesusG <helijesusg@gmail.com>
2016-11-08 14:29:52 -0400

    py1

--- primes.py
+++ primes.py
@@ -1,29 +1,27 @@
 """
 TECHNICAL EVALUATION - VAUXOO
-
 DEVELOPED BY:	HELI JESUS GONZALEZ TORRES
 """
-
 
 class PrimeClass(object):
     """
     Class used to determine if a number is prime.
-	
-	By definition, a number is prime only when it is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+	By definition, a number is prime only when it is a natural number
+	greater than 1 that has no positive divisors other than 1 and itself.
     """
-	
     def is_prime(self, num_int):
         """
-        This is a method that returns TRUE if the number received how to parameter is prime, FALSE otherwise.
+        This is a method that returns TRUE if the number received how to
+		parameter is prime, FALSE otherwise.
         """
-        # your primes code here
 		if num_int <= 1:
 			return False
 		
 		prime_num = True
 		for i in range(2,num_int):
-			if (num_int % i) == 0:	# If the division module is zero, then it is not prime.
+			# If the division module is zero, then it is not prime.
+			if (num_int % i) == 0:
 				prime_num = False
 				break
 		
-		return prime_num	# Remove this dummy line
+		return prime_num
