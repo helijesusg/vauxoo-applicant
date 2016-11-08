109d3cf9b05d6e32afacf8d2b0de04826521b680
HeliJesusG <helijesusg@gmail.com>
2016-11-08 14:01:23 -0400

    py2

--- calculator.py
+++ calculator.py
@@ -6,23 +6,17 @@ DEVELOPED BY:	HELI JESUS GONZALEZ TORRES
 
 
 class CalculatorClass(object):
-    """
-    Class used to calculate the sum of a list of numbers.
-    """
-
-    def sum(self, num_list):
-        """
-        Method to sum the list of numbers that is received in num_list
-        """
-        # your sum code here
+	"""
+	Class used to calculate the sum of a list of numbers.
+	"""
+	
+	def sum(self, num_list):
+		"""
+		Method to sum the list of numbers that is received in num_list
+		"""
+		#your sum code here
 		sum = 0
-		for i in range(0,len(num_list)):
+			for i in range(0,len(num_list)):
 			sum += num_list[i]
 		
-        return sum		# Remove this dummy line
-
-# Code section to test the class through a console
-calculator = CalculatorClass()							# The class is instantiated
-value = calculator.sum([1,2,3,4,5,6])					# Class method is called
-print value												# Console output output
-del calculator											# Instance is deleted
+	return sum		# Remove this dummy line
