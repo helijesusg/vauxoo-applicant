a285db31a1cbcb97bd375dbf9fc7f7aa8e067826
HeliJesusG <helijesusg@gmail.com>
2016-11-08 14:03:59 -0400

    py1

--- primes.py
+++ primes.py
@@ -8,10 +8,10 @@ DEVELOPED BY:	HELI JESUS GONZALEZ TORRES
 class PrimeClass(object):
     """
     Class used to determine if a number is prime.
-
+	
 	By definition, a number is prime only when it is a natural number greater than 1 that has no positive divisors other than 1 and itself.
     """
-
+	
     def is_prime(self, num_int):
         """
         This is a method that returns TRUE if the number received how to parameter is prime, FALSE otherwise.
@@ -25,14 +25,5 @@ class PrimeClass(object):
 			if (num_int % i) == 0:	# If the division module is zero, then it is not prime.
 				prime_num = False
 				break
-
+		
 		return prime_num	# Remove this dummy line
-
-# Code section to test the class through a console
-num_string = raw_input('Input number:')	# Entry number is requested
-numero = int(num_string)				# Parse from string to numeric
-
-prime = PrimeClass()					# The class is instantiated
-value = prime.is_prime(numero)			# Class method is called
-print value								# Console output output
-del prime								# Instance is deleted
