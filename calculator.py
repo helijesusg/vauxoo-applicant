512e7d0f77e49d6c6a562654670e25d8963aaf28
HeliJesusG <helijesusg@gmail.com>
2016-11-08 14:49:45 -0400

    py2

--- calculator.py
+++ calculator.py
@@ -3,16 +3,19 @@ TECHNICAL EVALUATION - VAUXOO
 DEVELOPED BY:	HELI JESUS GONZALEZ TORRES
 """
 
+
 class CalculatorClass(object):
 	"""
 	Class used to calculate the sum of a list of numbers.
 	"""
+	
 	def sum(self, num_list):
 		"""
 		Method to sum the list of numbers that is received in num_list
 		"""
+		
 		sum = 0
-		for i in range(0,len(num_list)):
+		for i in range(0, len(num_list)):
 			sum += num_list[i]
 		
 		return sum
